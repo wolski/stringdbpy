e38d1b73ba8ff72fa59b9aaa6f84c51b88e51573
B-Fabric Account <bfabric@bfabric.org>
2025-02-21 16:03:18 +0100

    A373: fix paths, and deactivate some lines

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 task_dir="$1"
-cp /home/bfabric/slurmworker/config/A373_DIANN/run.sh "$task_dir/.."
-ln -s /home/bfabric/slurmworker/config/A373_DIANN/testing_string_api.py "$task_dir/.."
-cp /home/bfabric/slurmworker/config/A373_DIANN/uv.lock "$task_dir/.."
-cp /home/bfabric/slurmworker/config/A373_DIANN/pyproject.toml "$task_dir/.."
+cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/run.sh "$task_dir/.."
+#ln -s /home/bfabric/slurmworker/config/A373_STRING_GSEA/testing_string_api.py "$task_dir/.."
+#cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/uv.lock "$task_dir/.."
+#cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/pyproject.toml "$task_dir/.."
 
 cd "$task_dir/.."
-uv run --verbose --locked --project "." testing_string_api.py
+#uv run --verbose --locked --project "." testing_string_api.py
+
