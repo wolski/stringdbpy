0fdc89b69c216f9f2fc13eec70df35d31430fcf7
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-02-21 12:02:59 +0100

    A373: simplify dispatch with the new resource-flow impl

--- dispatch.py
+++ dispatch.py
@@ -1,54 +1,22 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
-#     "bfabric",
+#     "polars",
 #     "bfabric-app-runner",
-#     "cyclopts",
-#     "pyyaml",
 # ]
+#
+# [tool.uv.sources]
+# bfabric-app-runner = { git = "https://github.com/fgcz/bfabricPy.git", subdirectory = "bfabric_app_runner", rev = "main" }
 # ///
-from bfabric import Bfabric
-from bfabric.entities import Resource
-from bfabric.experimental.workunit_definition import WorkunitDefinition
-from bfabric_app_runner.dispatch.dispatch_single_resource_flow import (
-    DispatchSingleResourceFlow,
-    ConfigDispatchSingleResourceFlow,
-)
-from pathlib import Path
-import yaml
-import cyclopts
+import polars as pl
+from bfabric_app_runner.dispatch.dispatch_resource_flow import ResourceDispatcherCLI
 
 
-class Dispatch(DispatchSingleResourceFlow):
-    def dispatch_job(
-        self, resources: list[Resource], workunit: WorkunitDefinition
-    ) -> Path:
-        chunk_dir = self._out_dir / "work"
-        chunk_dir.mkdir(exist_ok=True, parents=True)
-        inputs = [
-            {
-                "type": "bfabric_resource",
-                "id": resource["id"],
-                "filename": Path(resource["relativepath"]).name,
-            }
-            for resource in resources
-        ]
-        with (chunk_dir / "inputs.yml").open("w") as file:
-            yaml.safe_dump({"inputs": inputs}, file)
-        return chunk_dir
-
-
-app = cyclopts.App()
-
-
-@app.default
-def main(workunit_ref: Path | int, out_dir: Path):
-    client = Bfabric.from_config()
-    config = ConfigDispatchSingleResourceFlow(filter_suffix=".zip")
-    workunit_definition = WorkunitDefinition.from_ref(workunit_ref, client=client)
-    dispatch = Dispatch(client, config, out_dir)
-    dispatch.dispatch_workunit(workunit_definition)
-
+def dispatch_strategy(resources_df, workunit_definition):
+    resources_df = resources_df.filter(pl.col("filename").str.ends_with(".zip"))
+    extra_inputs = []
+    return resources_df, extra_inputs
+    
 
 if __name__ == "__main__":
-    app()
+    ResourceDispatcherCLI(dispatch_strategy).run()
