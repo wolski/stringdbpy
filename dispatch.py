bec08d4bb663955f567289eb538fb24db94a0b94
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-02-24 16:55:16 +0100

    Add all input parameters to params.yml

--- dispatch.py
+++ dispatch.py
@@ -14,9 +14,15 @@ from bfabric_app_runner.dispatch.dispatch_resource_flow import ResourceDispatche
 
 def dispatch_strategy(resources_df, workunit_definition):
     resources_df = resources_df.filter(pl.col("filename").str.ends_with(".zip"))
-    extra_inputs = []
+    extra_inputs = [
+        {
+            "type": "static_yaml",
+            "filename": "params.yml",
+            "data": workunit_definition.execution.raw_parameters,
+        }
+    ]
     return resources_df, extra_inputs
-    
+
 
 if __name__ == "__main__":
     ResourceDispatcherCLI(dispatch_strategy).run()
