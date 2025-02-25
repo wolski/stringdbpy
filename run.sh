33701fb19febcd62b5db078f1a88596617286305
Witold Wolski <wew@fgcz.ethz.ch>
2025-02-25 11:01:44 +0100

    A373 trying to fix working directory

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 task_dir="$1"
+
+echo $task_dir
 cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/run.sh "$task_dir/.."
 #ln -s /home/bfabric/slurmworker/config/A373_STRING_GSEA/testing_string_api.py "$task_dir/.."
 #cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/uv.lock "$task_dir/.."
 #cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/pyproject.toml "$task_dir/.."
-
+cd ../work
 uv run --verbose --locked --project /home/bfabric/slurmworker/config/A373_STRING_GSEA -m a373_string_gsea.run_string_gsea
