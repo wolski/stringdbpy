9f45fff9413a409440a959a0fb488c51e8edd480
B-Fabric Account <bfabric@bfabric.org>
2025-02-25 11:26:12 +0100

    A373 fix run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/run.sh "$task_dir/.."
 #ln -s /home/bfabric/slurmworker/config/A373_STRING_GSEA/testing_string_api.py "$task_dir/.."
 #cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/uv.lock "$task_dir/.."
 #cp /home/bfabric/slurmworker/config/A373_STRING_GSEA/pyproject.toml "$task_dir/.."
-cd ../work
+cd $task_dir
 uv run --verbose --locked --project /home/bfabric/slurmworker/config/A373_STRING_GSEA -m a373_string_gsea.run_string_gsea
