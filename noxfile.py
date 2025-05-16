e12b9a8d4d92c1e01eb30b3153760f7044f0abe4
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2025-05-16 09:58:26 +0200

    Setup noxfile and test dependencies

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+import nox
+
+
+nox.options.default_venv_backend = "uv"
+
+@nox.session(python="3.13")
+def test(session):
+    session.install(".[test]")
+    session.run("uv", "pip", "list")
+    session.run("pytest", "--durations=50", "tests")
